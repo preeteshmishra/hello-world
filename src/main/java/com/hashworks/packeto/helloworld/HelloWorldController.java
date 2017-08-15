4d0a64255524f945401e9f210e4f6d3851239f8d
preeteshmishra <preetesh_bit@yahoo.in>
2017-08-16 01:45:49 +0530

    1st commint

---
+++ src/main/java/com/hashworks/packeto/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.hashworks.packeto.helloworld;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+
+    String home() {
+        return "Hello World!";
+    }
+}
